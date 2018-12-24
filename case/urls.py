e4f1b14f0421349ad524eda848bf099c5aca50fb
admin <admin@example.com>
2018-12-24 11:03:35 +0900

    make search function

---
+++ case/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'case'
+urlpatterns = [
+    path('', views.pj_list, name='pj_list'),
+    path('project/<int:pk>', views.pj_detail, name='pj_detail'),
+]
--- case/views.py
+++ case/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Case, Company
+from django.db.models import Q
 
-# Create your views here.
+
+def pj_list(request):
+    keyword = request.GET.get("keyword1")
+    if keyword:
+        print(keyword)
+        projects = Case.objects.all()
+        projects = projects.filter(
+            Q(title__contains=keyword) | Q(text__contains=keyword)
+        )
+        projects = projects.order_by('created_date')
+        return render(request, 'case/pj_list.html', {'projects': projects})  # ここでrenderで返すと02,03ｍでいかない
+
+    keyword = request.GET.get("keyword2")
+    if keyword:
+        print(keyword)
+        projects = Case.objects.all()
+        projects = projects.filter(company=keyword)
+        projects = projects.order_by('created_date')
+        return render(request, 'case/pj_list.html', {'projects': projects})
+
+    keyword = request.GET.get("keyword3")
+    if keyword:
+        print(keyword)
+        projects = Case.objects.all()
+        projects = projects.filter(
+            Q(lower_cost__lte=keyword, upper_cost__gte=keyword) |
+            Q(lower_cost=None, upper_cost__gte=keyword) |
+            Q(lower_cost__lte=keyword, upper_cost=None)
+        )
+        projects = projects.order_by('created_date')
+        return render(request, 'case/pj_list.html', {'projects': projects})
+
+    projects = Case.objects.all().order_by('created_date')
+    return render(request, 'case/pj_list.html', {'projects': projects})
+
+
+def pj_detail(request, pk):
+    project = get_object_or_404(Case, pk=pk)
+    return render(request, 'case/pj_detail.html', {'project': project})
