c255da0c386ab744d677eb67182485e84a12933a
admin <admin@example.com>
2019-01-09 16:41:08 +0900

    edit auth

--- ses_project/urls.py
+++ ses_project/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('case.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
 ]
