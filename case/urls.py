2fec6c69915df9fdaca79c244f4582fe6db43239
admin <admin@example.com>
2019-01-11 13:41:16 +0900

    edit dropdown

--- case/urls.py
+++ case/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'case'
 urlpatterns = [
     path('', views.pj_list, name='pj_list'),
-    path('project/<int:pk>', views.pj_detail, name='pj_detail'),
+    path('project/<int:pk>', views.pj_detail, name='pj_detail'),  # SPAにしたため不要か
 ]
