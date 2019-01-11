3476d4d57263c771b6d24790fd7b52336572f8a1
admin <honda@k--s.com>
2019-01-11 17:56:08 +0900

    change comment

--- case/urls.py
+++ case/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'case'
 urlpatterns = [
     path('', views.pj_list, name='pj_list'),
-    path('project/<int:pk>', views.pj_detail, name='pj_detail'),  # SPAにしたため不要か
+    path('project/<int:pk>', views.pj_detail, name='pj_detail'),
     path('import/', views.CaseImport.as_view(), name='import'),
     path('export/', views.case_export, name='export'),
 ]
